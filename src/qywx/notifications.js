f66d1b95f6d8007d21d01147241b0030716bff87
chenzhipei <chenzhipei@hotoa.com>
2020-10-26 19:11:30 +0800

    集成首页通知到企业微信

--- src/qywx/notifications.js
+++ src/qywx/notifications.js
@@ -8,6 +8,7 @@ Meteor.startup(function(){
         Push.oldSend(options);
         try {
             let ref;
+            console.log("options.from------------: ",options);
             if (options.from !== 'workflow')
                 return;
             
@@ -27,16 +28,35 @@ Meteor.startup(function(){
             let service = space.services.qiyeweixin;
             let agentId = service.agentid;
             let spaceId = space._id;
-            let instanceId = options.payload.instance;
-            let instance = Creator.getCollection('instances').findOne({_id:instanceId});
+            let payload = options.payload;
+            let url = "";
+            let text = "";
             
-            let instanceUrl = steedosConfig.qywx.workflow_url + 'workflow/space/' + spaceId + '/inbox/' + instanceId;
+            // 审批流程
+            if (payload.instance){
+                url = payload.host + 'workflow/space/' + spaceId + '/inbox/' + payload.instance;
+                text = workflowPush(options,url);
+            }else{
+                url = payload.host + payload.url;
+            }
             
-            let text = "请审批 " + '<a href=' + instanceUrl + '>' + options.text + '</a>';
-            if(instance.state == "completed")
-                text = options.text
+            // 知识推送
+            if (payload.related_to.o == "cms_posts")
+                text = cms_postsPush(options,url);
             
-            console.log("text--------: ",text);
+            // 公告推送
+            if (payload.related_to.o == "announcements")
+                text = announcementsPush(options,url);
+            
+            // 任务推送
+            if (payload.related_to.o == "tasks")
+                text = tasksPush(options,url);
+            
+            // 日程推送
+            if (payload.related_to.o == "events")
+                text = eventsPush(options,url);
+            
+            
             let o = ServiceConfiguration.configurations.findOne({
                 service: "qiyeweixin"
             });
@@ -44,6 +64,7 @@ Meteor.startup(function(){
             if (at && at.access_token) {
                 service.access_token = at.access_token;
             }
+
             let msg = {
                 "touser" : qywx_userId,
                 "msgtype" : "text",
@@ -64,3 +85,57 @@ Meteor.startup(function(){
         }
     }
 })
+
+// 待审核推送
+let workflowPush = function(options,url){
+    if (!options || (options == {}))
+        return false;
+    
+    // 获取申请单
+    let instanceId = options.payload.instance;
+    let instance = Creator.getCollection('instances').findOne({_id:instanceId});
+    
+    let instanceUrl = options.payload.host + 'workflow/space/' + spaceId + '/inbox/' + instanceId;
+    
+    let text = "请审批 " + '<a href=' + instanceUrl + '>' + options.text + '</a>';
+    
+    if(instance.state == "completed")
+        text = options.text
+    
+    return text;
+}
+
+// 知识推送
+let cms_postsPush = function(options,url){
+    if (!options || (options == {}))
+        return false;
+    
+    let text = "【知识】\n" + '<a href=' + url + '>' + options.text + '</a>';
+        return text;    
+}
+
+// 公告推送
+let announcementsPush = function(options,url){
+    if (!options || (options == {}))
+        return false;
+    let text = "【公告通知】\n" + '<a href=' + url + '>' + options.text + '</a>';
+    return text;
+}
+
+// 任务推送
+let tasksPush = function(options,url){
+    if (!options || (options == {}))
+        return false;
+    
+    let text = "【任务通知】\n" + '<a href=' + url + '>' + options.text + '</a>';
+        return text;
+}
+
+// 日程推送
+let eventsPush = function(options,url){
+    if (!options || (options == {}))
+        return false;
+    
+    let text = "【日程通知】\n" + '<a href=' + url + '>' + options.text + '</a>';
+        return text;
+}
