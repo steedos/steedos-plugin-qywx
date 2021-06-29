da93ca937af00f5908405eb71cedae922d34f689
chenzhipei <chenzhipei@hotoa.com>
2021-06-29 13:34:16 +0800

    0.0.22

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = require('./qywx/api').router;
+const qywxPush = require('./qywx/notifications');
+      
 module.exports = {
     init: function () {
+        qywxPush.notify();
         let app = express();
         app.use('', router);
         WebApp.connectHandlers.use(app);
